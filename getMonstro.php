57d62e70b6bbcb4cf2adf2695aa9ca25910cc020
Hysumi <hysumi@gmail.com>
2016-08-29 04:55:38 -0300

    getMonstro funciona

---
+++ getMonstro.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <?php
+  	 if(@$_GET['Monstro_Nome'] != ''){
+    	 require_once "db/Monstro.class.php";
+    	 $monstro = new Monstro();
+       $ms = $monstro->get($_GET['Monstro_Nome']);
+       if($ms == null){
+    ?>
+        <h1>Sem resultados para: <?php echo $_GET['Monstro_Nome'];?></h1>
+        <?php
+          }else{
+        ?>
+            Nome do Monstro: <?php echo $ms->Monstro_Nome; ?><br>
+            Nivel: <?php echo $ms->Monstro_Nivel; ?><br>
+            Elemento: <?php echo $ms->Monstro_Elemento;?><br>
+            Tipo: <?php echo $ms->Monstro_Tipo;?><br>
+            <?php
+    	    }
+      }
+            ?>
+      <form method="get">
+        Buscar um Monstro: <br><br>
+        Nome do Monstro: <input type="text" name="Monstro_Nome"><br>
+
+        <input type="submit">
+      </form>
+  </body>
+</html>
