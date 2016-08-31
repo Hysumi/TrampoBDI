b2f1317ef92410623eb53389a3b75c15e6f5d07a
Hysumi <hysumi@gmail.com>
2016-08-31 03:42:21 -0300

    Quase pronto

--- custom.php
+++ custom.php
@@ -5,14 +5,13 @@
     <title></title>
   </head>
   <body>
-
     <form method="get">
       <br>Busca Customizada: <br><br>
       <textarea name="buscaCustom" cols = 60 rows = 20></textarea> <br>
       <input type="submit"><br>
       <a href="index.php">Voltar para página inicial.</a><br>
-
     </form>
+    
     <?php
   	 if(@$_GET['buscaCustom'] != ''){
     	 require_once "db/Custom.class.php";
